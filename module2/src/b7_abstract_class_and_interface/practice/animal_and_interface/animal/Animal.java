2994452256ffc982f67ba2ca0a5a4bc303a92910
LeBaThien <lethien6852@gmail.com>
2021-09-13 09:28:07 +0700

    13/9

--- module2/src/b7_abstract_class_and_interface/practice/animal_and_interface/animal/Animal.java
+++ module2/src/b7_abstract_class_and_interface/practice/animal_and_interface/animal/Animal.java
@@ -2,4 +2,5 @@ package b7_abstract_class_and_interface.practice.animal_and_interface.animal;
 
 public abstract class Animal {
         public abstract String makeSound();
+        
 }
--- module2/src/b7_abstract_class_and_interface/practice/animal_and_interface/animal/Chicken.java
+++ module2/src/b7_abstract_class_and_interface/practice/animal_and_interface/animal/Chicken.java
@@ -8,6 +8,7 @@ public class Chicken extends Animal implements Edible {
         return "Chicken: cluck-cluck!";
     }
 
+
     @Override
     public String howToEat() {
         return "could be fried";
