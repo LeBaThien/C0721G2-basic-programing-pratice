2496645742ba69f87cb62d5b85d136efd1b3b482
LeBaThien <lethien6852@gmail.com>
2021-12-17 17:47:51 +0700

    12/17

---
+++ module5/b3_angular_overview/bai_tap/angular-color-picker-app/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {BoxshadowComponent} from './boxshadow/boxshadow.component';
+import {GradientComponent} from './gradient/gradient.component';
+
+
+const routes: Routes = [
+  {
+    path: 'gradient',
+    component: GradientComponent
+  },
+  {
+    path: 'box-shadow',
+    component: BoxshadowComponent
+  },
+  { path: '',
+    redirectTo: 'gradient',
+    pathMatch: 'full'
+  },
+  { path: '**', component: GradientComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ module5/b3_angular_overview/bai_tap/angular-color-picker-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BoxshadowComponent } from './boxshadow/boxshadow.component';
+import {FormsModule} from '@angular/forms';
+import { GradientComponent } from './gradient/gradient.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BoxshadowComponent,
+    GradientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ColorPickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
