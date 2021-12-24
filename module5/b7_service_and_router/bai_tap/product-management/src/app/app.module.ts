7594e86b9251d0397438f347b66769903972a22e
LeBaThien <lethien6852@gmail.com>
2021-12-24 09:31:41 +0700

    12/24

--- module5/b7_service_and_router/bai_tap/product-management/src/app/app.module.ts
+++ module5/b7_service_and_router/bai_tap/product-management/src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCreateComponent,
+    ProductDeleteComponent,
+    ProductEditComponent,
+    ProductListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
